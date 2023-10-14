b60c8eecbd52a59a8f14f4fac059ba2ae5a3e727
Sharonn <sharonn.zounon@clarilog.com>
2023-10-14 13:30:46 +0200

    Init project

--- project/src/app/main.py
+++ project/src/app/main.py
@@ -3,12 +3,12 @@ from fastapi import FastAPI
 
 load_dotenv()
 
+# Deepgram
 app = FastAPI(
     title="Deepgram api management",
     description="ReadTheVoice API for managing the speech-to-text",
     version="1.0.0"
 )
-
 
 # @app.on_event("startup")
 # async def startup_event():
