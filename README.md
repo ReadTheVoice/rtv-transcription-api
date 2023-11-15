4376b0a6f525c48a8604ea6c69a3e749970370ca
Sharonn <123483236+SharonnElfride@users.noreply.github.com>
2023-11-15 14:57:33 +0100

    following deepgram tutorial

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # api
 
 Using Deepgram
+
+#### Run the app
+```shell
+# uvicorn main:app --reload
+# uvicorn project.src.app.main:app --reload
+uvicorn main:app --reload
+```
+
+#### How it works
+From browser to server (fastapi) & from server to Deepgram.
--- project/src/app/main.py
+++ project/src/app/main.py
@@ -1,14 +1,25 @@
+import os
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, WebSocket
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.templating import Jinja2Templates
+from deepgram import Deepgram
+from typing import Dict, Callable
 
 load_dotenv()
 
 # Deepgram
 app = FastAPI(
-    title="Deepgram api management",
-    description="ReadTheVoice API for managing the speech-to-text",
+    title="Live transcription with Deepgram API management",
+    description="ReadTheVoice API for managing speech-to-text functionality",
     version="1.0.0"
 )
+
+templates = Jinja2Templates(directory="project/src/app/templates")
+
+deepgram_client = Deepgram(os.getenv('DEEPGRAM_API_KEY'))
+
+favicon_path = 'project/src/app/static/img/favicon.ico'
 
 # token=...&meetingId=...
 
@@ -16,3 +27,71 @@ app = FastAPI(
 # async def startup_event():
 
 # app.include_router()
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def get_favicon():
+    return FileResponse(favicon_path)
+
+
+@app.get("/", response_class=HTMLResponse)
+def get(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+# Websocket Connection Between Server and Browser
+@app.websocket("/listen")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+
+    try:
+        deepgram_socket = await process_audio(websocket)
+
+        while True:
+            data = await websocket.receive_bytes()
+            deepgram_socket.send(data)
+            print("********************************************************************")
+            print(data)
+            print("********************************************************************")
+    except Exception as e:
+        raise Exception(f'Could not process audio: {e}')
+    finally:
+        await websocket.close()
+
+
+# Process the audio, get the transcript from that audio and connect to Deepgram.
+async def process_audio(fast_socket: WebSocket):
+    async def get_transcript(data: Dict) -> None:
+        if 'channel' in data:
+            transcript = data['channel']['alternatives'][0]['transcript']
+
+            print("********************************************************************")
+            print(transcript)
+            print("********************************************************************")
+
+            if transcript:
+                await fast_socket.send_text(transcript)
+
+        print("********************************************************************")
+        print(data)
+        print("********************************************************************")
+
+    deepgram_socket = await connect_to_deepgram(get_transcript)
+
+    return deepgram_socket
+
+
+# Connect to Deepgram.
+async def connect_to_deepgram(transcript_received_handler: Callable[[Dict], None]):
+    try:
+        socket = await dg_client.transcription.live({'punctuate': True, 'interim_results': False})
+        socket.registerHandler(socket.event.CLOSE, lambda c: print(f'Connection closed with code {c}.'))
+        socket.registerHandler(socket.event.TRANSCRIPT_RECEIVED, transcript_received_handler)
+
+        return socket
+    except Exception as e:
+        raise Exception(f'Could not open socket: {e}')
+
+
+
+
